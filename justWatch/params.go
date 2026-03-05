8d758f941a931f15ecdd778e45a245d4def67066
27 <27@riseup.net>
2026-03-04 21:52:15 -0600

    +   {"2026-03-04", "subId3", "justappsvod"},

--- justWatch/params.go
+++ justWatch/params.go
@@ -5,6 +5,26 @@ import (
    "strings"
 )
 
+var params_to_delete = []struct {
+   date  string
+   key   string
+   value string
+}{
+   {"2026-03-04", "subId3", "justappsvod"},
+   {"2026-02-26", "autoplay", "1"},
+   {"2026-02-26", "searchReferral", "publisher"},
+   {"2026-02-26", "source", "bing"},
+   {"2026-02-26", "source", "search-feeds"},
+   {"2026-02-26", "utm_campaign", "vod_feed"},
+   {"2026-02-26", "utm_content", ""},
+   {"2026-02-26", "utm_medium", "deeplink"},
+   {"2026-02-26", "utm_medium", "partner"},
+   {"2026-02-26", "utm_source", "justWatch-v2-catalog"},
+   {"2026-02-26", "utm_source", "justwatch"},
+   {"2026-02-26", "utm_source", "universal_search"},
+   {"2026-02-26", "utm_term", ""},
+}
+
 func getUrlGroupingKey(rawUrl string) string {
    trimmedUrl := strings.TrimSuffix(rawUrl, "\n")
    parsed, err := url.Parse(trimmedUrl)
@@ -24,23 +44,4 @@ func getUrlGroupingKey(rawUrl string) string {
    }
    parsed.RawQuery = query.Encode()
    return parsed.String()
-}
-
-var params_to_delete = []struct{
-   date string
-   key string
-   value string
-}{
-   {"2026-02-26", "autoplay", "1"},
-   {"2026-02-26", "searchReferral", "publisher"},
-   {"2026-02-26", "source", "bing"},
-   {"2026-02-26", "source", "search-feeds"},
-   {"2026-02-26", "utm_campaign", "vod_feed"},
-   {"2026-02-26", "utm_content", ""},
-   {"2026-02-26", "utm_medium", "deeplink"},
-   {"2026-02-26", "utm_medium", "partner"},
-   {"2026-02-26", "utm_source", "justWatch-v2-catalog"},
-   {"2026-02-26", "utm_source", "justwatch"},
-   {"2026-02-26", "utm_source", "universal_search"},
-   {"2026-02-26", "utm_term", ""},
 }
