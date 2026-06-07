fd4e95fc572747c8323575f4de1a72d349818e8b
27 <27@riseup.net>
2026-06-07 13:31:23 -0500

    +func extractExplicitValues(req *PartialEntry, cfg *TraceConfig) []string {

--- 2026/6/http-archive/extractor.go
+++ 2026/6/http-archive/extractor.go
@@ -3,95 +3,78 @@ package main
 import (
    "encoding/json"
    "fmt"
-   "net/url"
    "strings"
 )
 
-func extractPotentialTokens(req *PartialEntry) []string {
-   tokenMap := make(map[string]bool)
+// Accepts *TraceConfig
+func extractExplicitValues(req *PartialEntry, cfg *TraceConfig) []string {
+   var extracted []string
 
-   addToken := func(s string) {
-      if len(s) >= 8 {
-         tokenMap[s] = true
+   addValue := func(val string) {
+      val = strings.TrimSpace(val)
+      val = strings.TrimPrefix(val, "Bearer ")
+      if val != "" {
+         extracted = append(extracted, val)
       }
    }
 
-   for _, h := range req.Request.Headers {
-      val := strings.TrimPrefix(h.Value, "Bearer ")
-      addToken(val)
-   }
-
-   for _, c := range req.Request.Cookies {
-      addToken(c.Value)
-   }
-   for _, q := range req.Request.QueryString {
-      addToken(q.Value)
-   }
-
-   for _, p := range req.Request.PostData.Params {
-      addToken(p.Value)
-   }
-
-   text := req.Request.PostData.Text
-   if text != "" {
-      var jsonData interface{}
-      if err := json.Unmarshal([]byte(text), &jsonData); err == nil {
-         extractJSONValues(jsonData, addToken)
-      } else if strings.Contains(text, "=") {
-         if parsedForm, err := url.ParseQuery(text); err == nil {
-            for _, vals := range parsedForm {
-               for _, v := range vals {
-                  addToken(v)
-               }
-            }
+   for _, targetHeader := range cfg.Headers {
+      for _, h := range req.Request.Headers {
+         if strings.EqualFold(h.Name, targetHeader) {
+            addValue(h.Value)
          }
       }
    }
 
-   if parsedURL, err := url.Parse(req.Request.URL); err == nil {
-      segments := strings.Split(parsedURL.Path, "/")
-      for _, segment := range segments {
-         addToken(segment)
+   for _, targetQuery := range cfg.Queries {
+      for _, q := range req.Request.QueryString {
+         if strings.EqualFold(q.Name, targetQuery) {
+            addValue(q.Value)
+         }
       }
    }
 
-   var tokens []string
-   for k := range tokenMap {
-      tokens = append(tokens, k)
+   if len(cfg.JSONKeys) > 0 && req.Request.PostData.Text != "" {
+      var payload interface{}
+      if err := json.Unmarshal([]byte(req.Request.PostData.Text), &payload); err == nil {
+         for _, targetKey := range cfg.JSONKeys {
+            findJSONKey(payload, targetKey, addValue)
+         }
+      }
    }
-   return tokens
+
+   return extracted
 }
 
-func extractJSONValues(v interface{}, addToken func(string)) {
-   switch val := v.(type) {
+func findJSONKey(data interface{}, targetKey string, addValue func(string)) {
+   switch val := data.(type) {
    case map[string]interface{}:
-      for _, child := range val {
-         extractJSONValues(child, addToken)
+      for k, v := range val {
+         if strings.EqualFold(k, targetKey) {
+            switch castV := v.(type) {
+            case string:
+               addValue(castV)
+            case float64:
+               addValue(fmt.Sprintf("%v", castV))
+            case bool:
+               addValue(fmt.Sprintf("%t", castV))
+            }
+         }
+         findJSONKey(v, targetKey, addValue)
       }
    case []interface{}:
-      for _, child := range val {
-         extractJSONValues(child, addToken)
+      for _, item := range val {
+         findJSONKey(item, targetKey, addValue)
       }
-   case string:
-      addToken(val)
-   case float64:
-      addToken(fmt.Sprintf("%v", val))
    }
 }
 
-func responseContainsToken(res *PartialEntry, token string) bool {
-   if strings.Contains(res.Response.Content.Text, token) {
-      return true
-   }
+func responseContainsValue(res *PartialEntry, value string) bool {
    for _, h := range res.Response.Headers {
-      if strings.Contains(h.Value, token) {
+      if strings.Contains(h.Value, value) {
          return true
       }
    }
-   for _, c := range res.Response.Cookies {
-      if strings.Contains(c.Value, token) {
-         return true
-      }
-   }
-   return false
+
+   return strings.Contains(res.Response.Content.Text, value)
 }
--- 2026/6/http-archive/main.go
+++ 2026/6/http-archive/main.go
@@ -6,6 +6,7 @@ import (
    "fmt"
    "os"
    "sort"
+   "strings"
 )
 
 type HAR struct {
@@ -22,12 +23,10 @@ type PartialEntry struct {
    Request         struct {
       URL      string `json:"url"`
       PostData struct {
-         MimeType string          `json:"mimeType"`
-         Text     string          `json:"text"`
-         Params   []NameValuePair `json:"params"`
+         MimeType string `json:"mimeType"`
+         Text     string `json:"text"`
       } `json:"postData"`
       Headers     []NameValuePair `json:"headers"`
-      Cookies     []NameValuePair `json:"cookies"`
       QueryString []NameValuePair `json:"queryString"`
    } `json:"request"`
    Response struct {
@@ -35,7 +34,6 @@ type PartialEntry struct {
          Text string `json:"text"`
       } `json:"content"`
       Headers []NameValuePair `json:"headers"`
-      Cookies []NameValuePair `json:"cookies"`
    } `json:"response"`
 }
 
@@ -44,28 +42,62 @@ type NameValuePair struct {
    Value string `json:"value"`
 }
 
+// stringSlice allows accepting multiple flags of the same name (e.g. -req-header A -req-header B)
+type stringSlice []string
+
+func (s *stringSlice) String() string {
+   return strings.Join(*s, ", ")
+}
+
+func (s *stringSlice) Set(value string) error {
+   *s = append(*s, value)
+   return nil
+}
+
+type TraceConfig struct {
+   TargetTime string
+   InputFile  string
+   OutputFile string
+   Headers    []string
+   Queries    []string
+   JSONKeys   []string
+}
+
 func main() {
-   inputFile := flag.String("in", "", "Path to the input .har file (required)")
-   targetTime := flag.String("time", "", "startedDateTime value of the target request (required)")
-   outputFile := flag.String("out", "", "Path to the output .har file (required)")
+   var config TraceConfig
+   var headers, queries, jsonKeys stringSlice
+
+   flag.StringVar(&config.InputFile, "in", "", "Path to the input .har file (required)")
+   flag.StringVar(&config.TargetTime, "time", "", "startedDateTime value of the target request (required)")
+
+   flag.Var(&headers, "req-header", "Name of a Request Header to trace (can be specified multiple times)")
+   flag.Var(&queries, "req-query", "Name of a URL Query Parameter to trace (can be specified multiple times)")
+   flag.Var(&jsonKeys, "req-json", "Name of a JSON body key to trace (can be specified multiple times)")
 
    flag.Parse()
 
-   if *inputFile == "" || *targetTime == "" || *outputFile == "" {
-      fmt.Println("Error: Missing required arguments.")
-      fmt.Println("Usage:")
+   if config.InputFile == "" || config.TargetTime == "" {
+      fmt.Fprintln(os.Stderr, "Error: Missing required arguments.")
+      fmt.Fprintln(os.Stderr, "Usage:")
       flag.PrintDefaults()
       os.Exit(1)
    }
 
-   if err := processHAR(*inputFile, *targetTime, *outputFile); err != nil {
+   config.Headers = headers
+   config.Queries = queries
+   config.JSONKeys = jsonKeys
+   config.OutputFile = "output.har"
+
+   // Passed as a pointer
+   if err := processHAR(&config); err != nil {
       fmt.Fprintf(os.Stderr, "Fatal error: %v\n", err)
       os.Exit(1)
    }
 }
 
-func processHAR(inputFile, targetTime, outputFile string) error {
-   data, err := os.ReadFile(inputFile)
+// Accepts *TraceConfig
+func processHAR(cfg *TraceConfig) error {
+   data, err := os.ReadFile(cfg.InputFile)
    if err != nil {
       return fmt.Errorf("error reading file: %w", err)
    }
@@ -83,7 +115,7 @@ func processHAR(inputFile, targetTime, outputFile string) error {
          fmt.Printf("Error parsing entry %d: %v\n", i, err)
          continue
       }
-      if entries[i].StartedDateTime == targetTime {
+      if entries[i].StartedDateTime == cfg.TargetTime {
          targetIdx = i
       }
    }
@@ -95,22 +127,23 @@ func processHAR(inputFile, targetTime, outputFile string) error {
    queue := []int{targetIdx}
    kept := map[int]bool{targetIdx: true}
 
-   fmt.Printf("Target found at index %d. Tracing dependencies...\n", targetIdx)
+   fmt.Printf("Target found at index %d. Extracting configured keys and tracing...\n", targetIdx)
 
    for len(queue) > 0 {
       currIdx := queue[0]
       queue = queue[1:]
 
-      tokens := extractPotentialTokens(&entries[currIdx])
+      // Passed as a pointer
+      valuesToTrace := extractExplicitValues(&entries[currIdx], cfg)
 
-      for _, token := range tokens {
+      for _, val := range valuesToTrace {
          for i := currIdx - 1; i >= 0; i-- {
             if kept[i] {
                continue
             }
 
-            if responseContainsToken(&entries[i], token) {
-               fmt.Printf("  -> Found origin of token '%s...' in request idx %d\n", token[:min(10, len(token))], i)
+            if responseContainsValue(&entries[i], val) {
+               fmt.Printf("  -> Found origin of value '%s...' in response of request idx %d\n", val[:min(15, len(val))], i)
                kept[i] = true
                queue = append(queue, i)
                break
@@ -137,10 +170,10 @@ func processHAR(inputFile, targetTime, outputFile string) error {
       return fmt.Errorf("error marshalling output: %w", err)
    }
 
-   if err := os.WriteFile(outputFile, outData, 0644); err != nil {
+   if err := os.WriteFile(cfg.OutputFile, outData, 0644); err != nil {
       return fmt.Errorf("error writing to output file: %w", err)
    }
 
-   fmt.Printf("\nSuccess! Kept %d out of %d requests. Saved to %s\n", len(filteredEntries), len(entries), outputFile)
+   fmt.Printf("\nSuccess! Kept %d out of %d requests. Saved to %s\n", len(filteredEntries), len(entries), cfg.OutputFile)
    return nil
 }
