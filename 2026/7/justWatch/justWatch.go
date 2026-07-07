ad76a05719ae5af8cea7395ec4eaff08fe63004e
27 <27@riseup.net>
2026-07-07 15:25:57 -0500

    deleted:    2026/7/justWatch/main.go

--- 2026/7/justWatch/justWatch.go
+++ 2026/7/justWatch/justWatch.go
@@ -1,22 +1,113 @@
 // `presentationType` data seems to be incorrect in some cases. For example,
 // JustWatch reports this as SD: fetchtv.com.au/movie/details/19285
 // when the site itself reports as HD
-package justWatch
+package main
 
 import (
    "bytes"
    "cmp"
-   _ "embed"
    "encoding/base64"
    "encoding/json"
    "errors"
+   "flag"
+   "fmt"
+   "log"
    "maps"
    "net/http"
    "net/url"
+   "os"
+   "path"
    "slices"
    "strings"
+   "time"
+   _ "embed"
 )
 
+func main() {
+   log.SetFlags(log.Ltime)
+   err := new(client).do()
+   if err != nil {
+      log.Fatal(err)
+   }
+}
+
+type client struct {
+   address string
+   filters string
+   sleep   time.Duration
+}
+
+func (c *client) do() error {
+   flag.StringVar(&c.address, "a", "", "address")
+   flag.DurationVar(&c.sleep, "s", 99*time.Millisecond, "sleep")
+   flag.StringVar(&c.filters, "f", "BUY,CINEMA,FAST,RENT", "filters")
+   flag.Parse()
+
+   if c.address != "" {
+      return c.do_address()
+   }
+   flag.Usage()
+   return nil
+}
+
+func (c *client) do_address() error {
+   url_path, err := GetPath(c.address)
+   if err != nil {
+      return err
+   }
+   var content Content
+   err = content.Fetch(url_path)
+   if err != nil {
+      return err
+   }
+   var allEnrichedOffers []*EnrichedOffer
+   for _, tag := range content.HrefLangTags {
+      locale, ok := EnUs.Locale(&tag)
+      if !ok {
+         return errors.New("Locale")
+      }
+      log.Print(locale)
+      offers, err := tag.Offers(locale)
+      if err != nil {
+         return err
+      }
+      for _, offer := range offers {
+         allEnrichedOffers = append(allEnrichedOffers,
+            &EnrichedOffer{Locale: locale, Offer: &offer},
+         )
+      }
+      time.Sleep(c.sleep)
+   }
+   enrichedOffers := Deduplicate(allEnrichedOffers)
+   enrichedOffers = FilterOffers(
+      enrichedOffers, strings.Split(c.filters, ",")...,
+   )
+   sortedUrls, groupedOffers := GroupAndSortByUrl(enrichedOffers)
+   data := &bytes.Buffer{}
+   for i, address := range sortedUrls {
+      if i >= 1 {
+         data.WriteString("\n\n")
+      }
+      data.WriteString("## ")
+      data.WriteString(address)
+      for _, enriched := range groupedOffers[address] {
+         data.WriteByte('\n')
+         data.WriteString("\ncountry: ")
+         data.WriteString(enriched.Locale.Country)
+         data.WriteString("\nname: ")
+         data.WriteString(enriched.Locale.CountryName)
+         data.WriteString("\nmonetization: ")
+         data.WriteString(enriched.Offer.MonetizationType)
+         if enriched.Offer.ElementCount >= 1 {
+            data.WriteString("\ncount: ")
+            fmt.Fprint(data, enriched.Offer.ElementCount)
+         }
+      }
+   }
+   name := path.Base(url_path) + ".md"
+   log.Println("WriteFile", name)
+   return os.WriteFile(name, data.Bytes(), os.ModePerm)
+}
 // 2025-11-04
 var EnUs = Locales{
    {FullLocale: "en_US", Country: "US", CountryName: "United States"},
